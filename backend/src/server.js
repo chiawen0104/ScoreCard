f55ad72ff64f2db9ab772da832de1011569d7c7a
Chia-Wen Lu <qwe9887476@gmail.com>
2022-12-26 02:30:10 +0800

    update codes

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,9 +28,9 @@ app.get("/api", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, "../../frontend", "build")));
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
     app.get("/*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../../frontend", "build", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
     });
 }
 
